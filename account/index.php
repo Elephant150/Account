bacc2ccc7fecdc85dec8032f8c3857a735fb1ee3
Elephant150 <dmytruk150@gmail.com>
2019-11-03 19:37:08 +0200

    design for login

---
+++ account/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Authorization and registration</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+<form action="" method="post">
+    <label>Login:</label>
+    <input type="text" name="login" placeholder="Enter your login" required>
+    <label>Password:</label>
+    <input type="password" name="password" placeholder="Enter your password" required>
+    <button>Login</button>
+    <p>Create an account - <a href="#">Registration</a></p>
+</form>
+</body>
+</html>
