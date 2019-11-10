169a27bdbc36440c751ecf38a5fa2862f54cfbb4
Elephant150 <dmytruk150@gmail.com>
2019-11-10 17:46:01 +0200

    start oop version

--- account/vendor/connect.php
+++
@@ -1,4 +0,0 @@
-<?php
-//    ----------------------- connection to the DataBase
-$opt = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
-$dbh = new PDO('mysql:host=tom;dbname=blog', 'root', '');
---
+++ profile/vendor/Connect.php
@@ -0,0 +1,13 @@
+<?php
+
+
+class Connect
+{
+    private $host ;
+    private $user;
+    private $password;
+    private $db;
+    private $charset;
+    private $opt;
+    public $pdo;
+}
